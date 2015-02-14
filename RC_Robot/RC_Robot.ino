2cfd5abfa781d4bb99470cc13bc4c2c4604af00c
xinyichen <chenxinyicxy@hotmail.com>
2015-02-14 17:34:50 -0500

    adjusted code format

--- RC_Robot/RC_Robot.ino
+++ RC_Robot/RC_Robot.ino
@@ -132,182 +132,182 @@ void loop() {
 //        Serial.print("  Pres(mbar): ");
 //        Serial.println(pressure_abs);
 //        Serial.println();
+}
+
+
+void get_sensor_data() {
+    //Battery voltage---------------------------------------------
+    battery_voltage = analogRead(BATTERY_MEASUREMENT_PIN);
+    battery_voltage = battery_voltage / 1024.0 * 5.0 * 3.0 - 10;
+    //------------------------------------------------------------
+
+    //IMU---------------------------------------------------------
+    sensors_event_t accel_event;
+    sensors_event_t mag_event;
+    sensors_event_t bmp_event;
+    sensors_vec_t orientation;
+    /* Read the accelerometer and magnetometer */
+    accel.getEvent(&accel_event);
+    mag.getEvent(&mag_event);
+    /* Use the new fusionGetOrientation function to merge accel/mag data */
+    if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation)) {
+        /* 'orientation' should have valid .roll and .pitch fields */
+        roll = orientation.roll;
+        pitch = orientation.pitch;
+        yaw = orientation.heading;
+    }
+    bmp.getEvent(&bmp_event);
+    if (bmp_event.pressure) {
+        /* Get ambient temperature in C */
+        float temperature;
+        bmp.getTemperature(&temperature);
+        /* Convert atmospheric pressure, SLP and temp to altitude */
+        alt_IMU = bmp.pressureToAltitude(seaLevelPressure, bmp_event.pressure, temperature);
+        temperature_IMU = temperature;
+    }
+    //---------------------------------------------------------------
+
+    //Depth Sensor---------------------------------------------------
+    // To measure to higher degrees of precision use the following sensor settings:
+    // ADC_256
+    // ADC_512
+    // ADC_1024
+    // ADC_2048
+    // ADC_4096
+    temperature_c = sensor.getTemperature(CELSIUS, ADC_512);
+    temperature_f = sensor.getTemperature(FAHRENHEIT, ADC_512);
+    pressure_abs = sensor.getPressure(ADC_4096);
+    pressure_relative = sealevel(pressure_abs, base_altitude);
+    altitude_delta = altitude(pressure_abs, pressure_baseline);
+    //---------------------------------------------------------------
+}
+
+
+void motor_execute() {
+    uint8_t motorSpd1 = 0;
+    uint8_t motorSpd2 = 0;
+    uint8_t motorSpd3 = 0;
+    uint8_t motorSpd4 = 0;
+    uint8_t motorSpd5 = 0;
+
+    if (Motor1 >= 255) {
+        motorSpd4 = Motor1 - 255; //value [0,255]
+        motorSpd4 = map(motorSpd4, 0, 255, 30, 255); //eliminate motor dead end
+        if (Motor1 <= 258) {
+            motorSpd4 = 0;
+        } //joystick bias
+        Motor_Driver_1->setSpeed(motorSpd4);
+        Motor_Driver_1->run(FORWARD);
+    }
+    else {
+        motorSpd4 = 255 - Motor1;
+        motorSpd4 = map(motorSpd4, 0, 255, 30, 255); //eliminate motor dead end
+        if (Motor1 >= 252) {
+            motorSpd4 = 0;
+        }
+        Motor_Driver_1->setSpeed(motorSpd4);
+        Motor_Driver_1->run(BACKWARD);
     }
 
-
-    void get_sensor_data() {
-        //Battery voltage---------------------------------------------
-        battery_voltage = analogRead(BATTERY_MEASUREMENT_PIN);
-        battery_voltage = battery_voltage / 1024.0 * 5.0 * 3.0 - 10;
-        //------------------------------------------------------------
-
-        //IMU---------------------------------------------------------
-        sensors_event_t accel_event;
-        sensors_event_t mag_event;
-        sensors_event_t bmp_event;
-        sensors_vec_t orientation;
-        /* Read the accelerometer and magnetometer */
-        accel.getEvent(&accel_event);
-        mag.getEvent(&mag_event);
-        /* Use the new fusionGetOrientation function to merge accel/mag data */
-        if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation)) {
-            /* 'orientation' should have valid .roll and .pitch fields */
-            roll = orientation.roll;
-            pitch = orientation.pitch;
-            yaw = orientation.heading;
+    if (Motor2 >= 255) {
+        motorSpd5 = Motor2 - 255;
+        motorSpd5 = map(motorSpd5, 0, 255, 30, 255); //eliminate motor dead end
+        if (Motor2 <= 258) {
+            motorSpd5 = 0;
         }
-        bmp.getEvent(&bmp_event);
-        if (bmp_event.pressure) {
-            /* Get ambient temperature in C */
-            float temperature;
-            bmp.getTemperature(&temperature);
-            /* Convert atmospheric pressure, SLP and temp to altitude */
-            alt_IMU = bmp.pressureToAltitude(seaLevelPressure, bmp_event.pressure, temperature);
-            temperature_IMU = temperature;
+        Motor_Driver_3->setSpeed(motorSpd5);
+        Motor_Driver_3->run(FORWARD);
+    }
+    else {
+        motorSpd5 = 255 - Motor2;
+        motorSpd5 = map(motorSpd5, 0, 255, 30, 255); //eliminate motor dead end
+        if (Motor2 >= 252) {
+            motorSpd5 = 0;
         }
-        //---------------------------------------------------------------
 
-        //Depth Sensor---------------------------------------------------
-        // To measure to higher degrees of precision use the following sensor settings:
-        // ADC_256
-        // ADC_512
-        // ADC_1024
-        // ADC_2048
-        // ADC_4096
-        temperature_c = sensor.getTemperature(CELSIUS, ADC_512);
-        temperature_f = sensor.getTemperature(FAHRENHEIT, ADC_512);
-        pressure_abs = sensor.getPressure(ADC_4096);
-        pressure_relative = sealevel(pressure_abs, base_altitude);
-        altitude_delta = altitude(pressure_abs, pressure_baseline);
-        //---------------------------------------------------------------
+        Motor_Driver_3->setSpeed(motorSpd5);
+        Motor_Driver_3->run(BACKWARD);
     }
 
-
-    void motor_execute() {
-        uint8_t motorSpd1 = 0;
-        uint8_t motorSpd2 = 0;
-        uint8_t motorSpd3 = 0;
-        uint8_t motorSpd4 = 0;
-        uint8_t motorSpd5 = 0;
-
-        if (Motor1 >= 255) {
-            motorSpd4 = Motor1 - 255; //value [0,255]
-            motorSpd4 = map(motorSpd4, 0, 255, 30, 255); //eliminate motor dead end
-            if (Motor1 <= 258) {
-                motorSpd4 = 0;
-            } //joystick bias
-            Motor_Driver_1->setSpeed(motorSpd4);
-            Motor_Driver_1->run(FORWARD);
-        }
-        else {
-            motorSpd4 = 255 - Motor1;
-            motorSpd4 = map(motorSpd4, 0, 255, 30, 255); //eliminate motor dead end
-            if (Motor1 >= 252) {
-                motorSpd4 = 0;
-            }
-            Motor_Driver_1->setSpeed(motorSpd4);
-            Motor_Driver_1->run(BACKWARD);
-        }
-
-        if (Motor2 >= 255) {
-            motorSpd5 = Motor2 - 255;
-            motorSpd5 = map(motorSpd5, 0, 255, 30, 255); //eliminate motor dead end
-            if (Motor2 <= 258) {
-                motorSpd5 = 0;
-            }
-            Motor_Driver_3->setSpeed(motorSpd5);
-            Motor_Driver_3->run(FORWARD);
-        }
-        else {
-            motorSpd5 = 255 - Motor2;
-            motorSpd5 = map(motorSpd5, 0, 255, 30, 255); //eliminate motor dead end
-            if (Motor2 >= 252) {
-                motorSpd5 = 0;
-            }
-
-            Motor_Driver_3->setSpeed(motorSpd5);
-            Motor_Driver_3->run(BACKWARD);
-        }
-
-            Motor3 -= 255;
-            if (Motor3 <= -10) {
-                Motor_Driver_4->setSpeed(-1 * Motor3 * 0.7);
-                Motor_Driver_4->run(BACKWARD);
-            } else if (Motor3 >= 10){
-                Motor_Driver_4->setSpeed(Motor3 * 0.7);
-                Motor_Driver_4->run(FORWARD);
-            } else {
-                Motor_Driver_4->setSpeed(0);
-            }
+    Motor3 -= 255;
+    if (Motor3 <= -10) {
+        Motor_Driver_4->setSpeed(-1 * Motor3 * 0.7);
+        Motor_Driver_4->run(BACKWARD);
+    } else if (Motor3 >= 10) {
+        Motor_Driver_4->setSpeed(Motor3 * 0.7);
+        Motor_Driver_4->run(FORWARD);
+    } else {
+        Motor_Driver_4->setSpeed(0);
     }
+}
 
 
-    void serial_2_send_data() {
-        Serial2.flush();
-        Serial2.print("#");
-        Serial2.print(battery_voltage, 3);
-        Serial2.print(",");
-        Serial2.print(normalized_joystick_Y, 3);
-        Serial2.print(",");
-        Serial2.print(joystick_button);
-        Serial2.print(",");
-        Serial2.print(Motor1 / 100);
-        Serial2.print((Motor1 - (Motor1 / 100) * 100) / 10);
-        Serial2.print(Motor1 % 10);
-        Serial2.print(",");
-        Serial2.print(Motor2 / 100);
-        Serial2.print((Motor2 - (Motor2 / 100) * 100) / 10);
-        Serial2.print(Motor2 % 10);
-        Serial2.print(",");
-        Serial2.print(Motor3 / 100);
-        Serial2.print((Motor3 - (Motor3 / 100) * 100) / 10);
-        Serial2.print(Motor3 % 10);
-        Serial2.print(",");
-        Serial2.print(Motor4 / 100);
-        Serial2.print((Motor4 - (Motor4 / 100) * 100) / 10);
-        Serial2.print(Motor4 % 10);
-        Serial2.print(",");
-        Serial2.print(Motor5 / 100);
-        Serial2.print((Motor5 - (Motor5 / 100) * 100) / 10);
-        Serial2.print(Motor5 % 10);
-        Serial2.print(",");
-        Serial2.print(checksum, 3);
+void serial_2_send_data() {
+    Serial2.flush();
+    Serial2.print("#");
+    Serial2.print(battery_voltage, 3);
+    Serial2.print(",");
+    Serial2.print(normalized_joystick_Y, 3);
+    Serial2.print(",");
+    Serial2.print(joystick_button);
+    Serial2.print(",");
+    Serial2.print(Motor1 / 100);
+    Serial2.print((Motor1 - (Motor1 / 100) * 100) / 10);
+    Serial2.print(Motor1 % 10);
+    Serial2.print(",");
+    Serial2.print(Motor2 / 100);
+    Serial2.print((Motor2 - (Motor2 / 100) * 100) / 10);
+    Serial2.print(Motor2 % 10);
+    Serial2.print(",");
+    Serial2.print(Motor3 / 100);
+    Serial2.print((Motor3 - (Motor3 / 100) * 100) / 10);
+    Serial2.print(Motor3 % 10);
+    Serial2.print(",");
+    Serial2.print(Motor4 / 100);
+    Serial2.print((Motor4 - (Motor4 / 100) * 100) / 10);
+    Serial2.print(Motor4 % 10);
+    Serial2.print(",");
+    Serial2.print(Motor5 / 100);
+    Serial2.print((Motor5 - (Motor5 / 100) * 100) / 10);
+    Serial2.print(Motor5 % 10);
+    Serial2.print(",");
+    Serial2.print(checksum, 3);
 
-        Serial2.print("\n");
+    Serial2.print("\n");
+}
+
+void initSensors() {
+    if (!accel.begin()) {
+        /* There was a problem detecting the LSM303 ... check your connections */
+        Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+        while (1);
     }
-
-    void initSensors() {
-        if (!accel.begin()) {
-            /* There was a problem detecting the LSM303 ... check your connections */
-            Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
-            while (1);
-        }
-        if (!mag.begin()) {
-            /* There was a problem detecting the LSM303 ... check your connections */
-            Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-            while (1);
-        }
-        if (!bmp.begin()) {
-            /* There was a problem detecting the BMP180 ... check your connections */
-            Serial.println("Ooops, no BMP180 detected ... Check your wiring!");
-            while (1);
-        }
+    if (!mag.begin()) {
+        /* There was a problem detecting the LSM303 ... check your connections */
+        Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+        while (1);
     }
+    if (!bmp.begin()) {
+        /* There was a problem detecting the BMP180 ... check your connections */
+        Serial.println("Ooops, no BMP180 detected ... Check your wiring!");
+        while (1);
+    }
+}
 
-    double sealevel(double P, double A)
+double sealevel(double P, double A)
 // Given a pressure P (mbar) taken at a specific altitude (meters),
 // return the equivalent pressure (mbar) at sea level.
 // This produces pressure readings that can be used for weather measurements.
-    {
-        return (P / pow(1 - (A / 44330.0), 5.255));
-    }
+{
+    return (P / pow(1 - (A / 44330.0), 5.255));
+}
 
 
-    double altitude(double P, double P0)
+double altitude(double P, double P0)
 // Given a pressure measurement P (mbar) and the pressure at a baseline P0 (mbar),
 // return altitude (meters) above baseline.
-    {
-        return (44330.0 * (1 - pow(P / P0, 1 / 5.255)));
-    }
+{
+    return (44330.0 * (1 - pow(P / P0, 1 / 5.255)));
+}
   
 
