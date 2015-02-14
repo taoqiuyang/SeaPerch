47f74104f80adb6f09802d8000b7288107dab6e3
Qiuyang Tao <taoqiuyang@163.com>
2015-02-14 01:23:23 -0500

    Add Depth Sensor

--- RC_Robot/RC_Robot.ino
+++ RC_Robot/RC_Robot.ino
@@ -13,9 +13,15 @@ SeaPerch Remote Control Test
  ================= How to use ===============================================
  
  */
+//I2C lib
 #include <Wire.h>
-//Motor Libs-------------------
+
+//Motor Driver Libs------------
 #include <Adafruit_MotorShield.h>
+//-----------------------------
+
+//Depth Sensor Libs------------
+#include <MS5803_I2C.h>
 //-----------------------------
 
 //IMU Libs---------------------
@@ -37,6 +43,16 @@ float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
 double yaw,pitch,roll,temperature_IMU,alt_IMU;
 //--------------------------------------------------------------------------
 
+//Depth Sensor--------------------------------------------------------------
+MS5803 sensor(ADDRESS_HIGH); //ADDRESS_HIGH=0x76 ADDRESS_LOW=0x77 default is HIGH  
+float temperature_c, temperature_f;
+double pressure_abs, pressure_relative, altitude_delta, pressure_baseline;
+// Create Variable to store altitude in (m) for calculations;
+double base_altitude = 1655.0; // Altitude of SparkFun's HQ in Boulder, CO. in (m)
+//--------------------------------------------------------------------------
+
+
+//Motors--------------------------------------------------------------------
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); //attach the board with 3 motors
 Adafruit_DCMotor *Motor_Driver_1 = AFMS.getMotor(1);
 Adafruit_DCMotor *Motor_Driver_2 = AFMS.getMotor(2);
@@ -48,7 +64,7 @@ int Motor2 = 255;
 int Motor3 = 255;
 int Motor4 = 255;
 int Motor5 = 255;
-
+//--------------------------------------------------------------------------
 
 String Serial_1_data_recieved = "";
 String Serial_2_data_recieved = "";
@@ -83,7 +99,12 @@ void setup() {
   //---initialize the IMU------------
   initSensors();
   //---------------------------------
-
+  
+  //---initialize Depth Sensor-------
+  sensor.reset();
+  sensor.begin();
+  pressure_baseline = sensor.getPressure(ADC_4096);
+  //---------------------------------
 }
 
 
@@ -117,10 +138,16 @@ void loop() {
     get_sensor_data();
     Serial.print("roll: ");
     Serial.print(roll);
-    Serial.print("   pitch: ");
+    Serial.print("  pitch: ");
     Serial.print(pitch);
-    Serial.print("   yaw: ");
+    Serial.print("  yaw: ");
     Serial.print(yaw);
+    Serial.print("  temp_IMU:");
+    Serial.print(temperature_IMU);
+    Serial.print("  Temp Dep: ");
+    Serial.print(temperature_c);
+    Serial.print("  Pres(mbar): ");
+    Serial.println(pressure_abs);
     Serial.println();
 
   delay(20);
@@ -138,11 +165,9 @@ void get_sensor_data() {
   sensors_event_t mag_event;
   sensors_event_t bmp_event;
   sensors_vec_t   orientation;
-
   /* Read the accelerometer and magnetometer */
   accel.getEvent(&accel_event);
   mag.getEvent(&mag_event);
-
   /* Use the new fusionGetOrientation function to merge accel/mag data */
   if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation))
   {
@@ -151,7 +176,6 @@ void get_sensor_data() {
     pitch=orientation.pitch;
     yaw=orientation.heading;
   }
-
   bmp.getEvent(&bmp_event);
   if (bmp_event.pressure)
   {
@@ -162,6 +186,20 @@ void get_sensor_data() {
     alt_IMU=bmp.pressureToAltitude(seaLevelPressure,bmp_event.pressure,temperature); 
     temperature_IMU=temperature;
   }
+  //---------------------------------------------------------------
+  
+  //Depth Sensor---------------------------------------------------
+  // To measure to higher degrees of precision use the following sensor settings:
+  // ADC_256 
+  // ADC_512 
+  // ADC_1024
+  // ADC_2048
+  // ADC_4096
+  temperature_c = sensor.getTemperature(CELSIUS, ADC_512);
+  temperature_f = sensor.getTemperature(FAHRENHEIT, ADC_512);
+  pressure_abs = sensor.getPressure(ADC_4096);
+  pressure_relative = sealevel(pressure_abs, base_altitude);
+  altitude_delta = altitude(pressure_abs , pressure_baseline);
   //---------------------------------------------------------------
 }
 
@@ -281,5 +319,20 @@ void initSensors()
   }
 }
 
+ double sealevel(double P, double A)
+// Given a pressure P (mbar) taken at a specific altitude (meters),
+// return the equivalent pressure (mbar) at sea level.
+// This produces pressure readings that can be used for weather measurements.
+{
+	return(P/pow(1-(A/44330.0),5.255));
+}
 
+
+double altitude(double P, double P0)
+// Given a pressure measurement P (mbar) and the pressure at a baseline P0 (mbar),
+// return altitude (meters) above baseline.
+{
+	return(44330.0*(1-pow(P/P0,1/5.255)));
+}
+  
 
