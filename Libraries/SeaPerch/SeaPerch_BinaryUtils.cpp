b8ba790553f2f8a0ee9e269db12576f921de2105
xinyichen <chenxinyicxy@hotmail.com>
2015-02-25 14:51:11 -0500

    const correctness for BinaryUtils

--- Libraries/SeaPerch/SeaPerch_BinaryUtils.cpp
+++ Libraries/SeaPerch/SeaPerch_BinaryUtils.cpp
@@ -1,13 +1,13 @@
 #include "SeaPerch_BinaryUtils.h"
 
-void toBytes(int anInt, char *buffer) {
+void toBytes(const int anInt, char *buffer) {
     for (int i = 0; i < INT_SIZE; i++) {
         char currentByte = (anInt >> (8 * i)) & BYTE_MASK;
         buffer[INT_SIZE - i - 1] = currentByte;
     }
 }
 
-int toInt(char *buffer) {
+int toInt(const char * buffer) {
     int integer = 0;
 
     for (int i = 0; i < INT_SIZE; i++) {
--- Libraries/SeaPerch/SeaPerch_BinaryUtils.h
+++ Libraries/SeaPerch/SeaPerch_BinaryUtils.h
@@ -5,7 +5,7 @@ const int BYTE_MASK = 0xff;
 const int INT_SIZE = sizeof(int);
 const int DOUBLE_SIZE = sizeof(short);
 
-void toBytes(int anInt, char *buffer);
-int toInt(char *buffer);
+void toBytes(const int anInt, char *buffer);
+int toInt(const char * buffer);
 
 #endif
