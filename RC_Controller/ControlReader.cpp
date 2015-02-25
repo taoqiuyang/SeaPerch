76de53975a777c658ff13b690d6da81799031722
xinyichen <chenxinyicxy@hotmail.com>
2015-02-25 00:10:57 -0500

    ControlReader cpp

---
+++ RC_Controller/ControlReader.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+#include "ControlPin.h"
+#include "ControlReader.h"
+
+ControlReader::ControlReader() : joystickMidPointX(0), joystickMidPointY(0) {
+}
+
+void ControlReader::calibrate() {
+    for (int i = 0; i < 200; i++) {
+        joystickMidPointX = (joystickMidPointX + analogRead(JOYSTICK_HORIZONTAL)) / 2;
+        delay(1);
+        joystickMidPointY = (joystickMidPointY + analogRead(JOYSTICK_VERTICAL)) / 2;
+        delay(1);
+    }
+}
+
+double ControlReader::getNormalizedJoystickX() {
+    return processJoystick(JOYSTICK_HORIZONTAL, joystickMidPointX);
+}
+
+double ControlReader::getNormalizedJoystickY() {
+    return processJoystick(JOYSTICK_VERTICAL, joystickMidPointY);
+}
+
+double ControlReader::processJoystick(int pinId, int midPoint) {
+    double normalized;
+    int rawValue = analogRead(pinId);
+
+    //-----------------normalize----------------
+    //map the ADC reading to [-1, 1]
+    if (rawValue < midPoint - 1) {
+        normalized = 1.0 * (midPoint - rawValue) / midPoint;
+    }
+    if (rawValue > midPoint + 1) {
+        normalized = -1.0 * (rawValue - midPoint) / (1023 - midPoint);
+    }
+    if (normalized > 1) {
+        normalized = 1;
+    }
+    if (normalized < -1) {
+        normalized = -1;
+    }
+    //-----------------apply curve--------------
+    return normalized + 1;
+}
--- RC_Controller/ControlReader.h
+++ RC_Controller/ControlReader.h
@@ -1,56 +1,21 @@
 #ifndef _ControlReader_H
 #define _ControlReader_H
 
-#include <Arduino.h>
-#include "ControlPin.h"
-
 class ControlReader {
 public:
-    ControlReader() : joystickMidPointX(0), joystickMidPointY(0) {
-    }
+    ControlReader();
 
-    void calibrate() {
-        for (int i = 0; i < 200; i++) {
-            joystickMidPointX = (joystickMidPointX + analogRead(JOYSTICK_HORIZONTAL)) / 2;
-            delay(1);
-            joystickMidPointY = (joystickMidPointY + analogRead(JOYSTICK_VERTICAL)) / 2;
-            delay(1);
-        }
-    }
+    void calibrate();
 
-    double getNormalizedJoystickX() {
-        return processJoystick(JOYSTICK_HORIZONTAL, joystickMidPointX);
-    }
+    double getNormalizedJoystickX();
 
-    double getNormalizedJoystickY() {
-        return processJoystick(JOYSTICK_VERTICAL, joystickMidPointY);
-    }
+    double getNormalizedJoystickY();
 
 private:
     int joystickMidPointX;
     int joystickMidPointY;
 
-    double processJoystick(int pinId, int midPoint) {
-        double normalized;
-        int rawValue = analogRead(pinId);
-
-        //-----------------normalize----------------
-        //map the ADC reading to [-1, 1]
-        if (rawValue < midPoint - 1) {
-            normalized = 1.0 * (midPoint - rawValue) / midPoint;
-        }
-        if (rawValue > midPoint + 1) {
-            normalized = -1.0 * (rawValue - midPoint) / (1023 - midPoint);
-        }
-        if (normalized > 1) {
-            normalized = 1;
-        }
-        if (normalized < -1) {
-            normalized = -1;
-        }
-        //-----------------apply curve--------------
-        return normalized + 1;
-    }
+    double processJoystick(int pinId, int midPoint);
 };
 
 #endif
