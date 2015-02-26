9ec0a07eb6c4ae55c1571ccf5fe211cfc601a537
xinyichen <chenxinyicxy@hotmail.com>
2015-02-26 00:53:25 -0500

    removed all motor value related statements

--- Libraries/SeaPerch/SeaPerch_ControlSpecs.cpp
+++ Libraries/SeaPerch/SeaPerch_ControlSpecs.cpp
@@ -1,23 +1,12 @@
 #include "SeaPerch_ControlSpecs.h"
 
-ControlSpecs::ControlSpecs(unsigned int motorCount) : motorCount(motorCount) {
-    motors = new int[motorCount];
+ControlSpecs::ControlSpecs(){
 }
 
 ControlSpecs::ControlSpecs(const ControlSpecs& controlSpecs) {
-    motorCount = controlSpecs.getMotorCount();
-    joystick_button = controlSpecs.getJoystick_button();
+    slidePot = controlSpecs.getSlidePot();
     normalized_joystick_X = controlSpecs.getNormalized_joystick_X();
     normalized_joystick_Y = controlSpecs.getNormalized_joystick_Y();
-
-    motors = new int[motorCount];
-    for (int i = 0; i < motorCount; ++i) {
-        motors[i] = controlSpecs.getMotor(i);
-    }
-}
-
-ControlSpecs::~ControlSpecs() {
-    delete[] motors;
 }
 
 const float ControlSpecs::getNormalized_joystick_X() const {
@@ -36,30 +25,10 @@ void ControlSpecs::setNormalized_joystick_Y(const float aNormalized_joystick_Y) 
     normalized_joystick_Y = aNormalized_joystick_Y;
 }
 
-const int ControlSpecs::getJoystick_button() const {
-    return joystick_button;
-}
-
-void ControlSpecs::setJoystick_button(const int aJoystick_button) {
-    joystick_button = aJoystick_button;
-}
-
 const int ControlSpecs::getSlidePot() const {
     return slidePot;
 }
 
 void ControlSpecs::setSlidePot(const int aSlidePot) {
     slidePot = aSlidePot;
-}
-
-const int ControlSpecs::getMotor(int motorId) const {
-    return motors[motorId];
-}
-
-void ControlSpecs::setMotor(int motorId, int motorValue) {
-    motors[motorId] = motorValue;
-}
-
-const unsigned int ControlSpecs::getMotorCount() const {
-    return motorCount;
 }
--- Libraries/SeaPerch/SeaPerch_ControlSpecs.h
+++ Libraries/SeaPerch/SeaPerch_ControlSpecs.h
@@ -3,11 +3,9 @@
 
 class ControlSpecs {
 public:
-    explicit ControlSpecs(unsigned int motorCount);
+    explicit ControlSpecs();
 
     ControlSpecs(const ControlSpecs& controlSpecs);
-
-    ~ControlSpecs();
 
     const float getNormalized_joystick_X() const;
 
@@ -17,24 +15,11 @@ public:
 
     void setNormalized_joystick_Y(const float aNormalized_joystick_Y);
 
-    const int getJoystick_button() const;
-
-    void setJoystick_button(const int aJoystick_button);
-
     const int getSlidePot() const;
 
     void setSlidePot(const int aSlidePot);
 
-    const int getMotor(int motorId) const;
-
-    void setMotor(int motorId, int motorValue);
-
-    const unsigned int getMotorCount() const;
-
 private:
-    int *motors;
-    int motorCount;
-    int joystick_button;
     int slidePot;
     float normalized_joystick_X;
     float normalized_joystick_Y;
