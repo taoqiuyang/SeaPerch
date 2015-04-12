a57c19664e1dcfcf4e7f8f04e244c874b4f338ad
xinyichen <chenxinyicxy@hotmail.com>
2015-04-12 14:46:16 -0400

    algo utils forgot to commit

--- libs/SeaPerch/SeaPerch_AlgoUtils.cpp
+++ libs/SeaPerch/SeaPerch_AlgoUtils.cpp
@@ -1,14 +1,14 @@
 #include "SeaPerch_AlgoUtils.h"
 
-double AlgoUtils::map(int value, int fromLow, int fromHigh, double toLow, double toHigh) {
+constexpr double AlgoUtils::map(int value, int fromLow, int fromHigh, double toLow, double toHigh) {
     return map((double)value, (double)fromLow, (double)fromHigh, toLow, toHigh);
 }
 
-double AlgoUtils::map(double value, int fromLow, int fromHigh, int toLow, int toHigh) {
-    return map(value, (double)fromLow, (double)fromHigh, (double)toLow, (double)toHigh);
+constexpr double AlgoUtils::map(float value, int fromLow, int fromHigh, int toLow, int toHigh) {
+    return map((double)value, (double)fromLow, (double)fromHigh, (double)toLow, (double)toHigh);
 }
 
-double AlgoUtils::map(double value, double fromLow, double fromHigh, double toLow, double toHigh) {
+constexpr double AlgoUtils::map(double value, double fromLow, double fromHigh, double toLow, double toHigh) {
     if (value <= fromLow) {
         return toLow;
     }
